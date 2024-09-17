fee62ac223b8c65426a61709d27513b5c21e020b
Akash Vaidya <akash.vaidya23@gmail.com>
2024-09-17 23:08:54 +0530

    some styling changes in list products.

--- src/Components/Product_List/Product_List.jsx
+++ src/Components/Product_List/Product_List.jsx
@@ -24,34 +24,69 @@ const Product_List = () => {
   return (
     <>
       <div>
-        <h1>Product List</h1>
-        <Paper sx={{ width: "100%", overflow: "hidden" }}>
-          <TableContainer sx={{ maxHeight: 750 }}>
-            <Table stickyHeader aria-label="sticky table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Sr. No</TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell>SKU</TableCell>
-                  <TableCell>Price</TableCell>
+        <h1 style={{ textAlign: "center" }}>Product List</h1>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <TableCell
+                style={{
+                  border: "1px solid black",
+                  textAlign: "center",
+                }}
+              >
+                Sr. No
+              </TableCell>
+              <TableCell
+                style={{ border: "1px solid black", textAlign: "center" }}
+              >
+                Name
+              </TableCell>
+              <TableCell
+                style={{ border: "1px solid black", textAlign: "center" }}
+              >
+                SKU
+              </TableCell>
+              <TableCell
+                style={{ border: "1px solid black", textAlign: "center" }}
+              >
+                Price
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product, index) => {
+              console.log(product);
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={index}>
+                  <TableCell
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                    key={index}
+                  >
+                    {index + 1}
+                  </TableCell>
+                  <TableCell
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                    key={index}
+                  >
+                    {product.title}
+                  </TableCell>
+                  <TableCell
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                    key={index}
+                  >
+                    {product.sku}
+                  </TableCell>
+                  <TableCell
+                    style={{ border: "1px solid black", textAlign: "center" }}
+                    key={index}
+                  >
+                    {product.price}
+                  </TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {products.map((product, index) => {
-                  console.log(product);
-                  return (
-                    <TableRow hover role="checkbox" tabIndex={-1} key={index}>
-                      <TableCell key={index}>{index + 1}</TableCell>
-                      <TableCell key={index}>{product.title}</TableCell>
-                      <TableCell key={index}>{product.sku}</TableCell>
-                      <TableCell key={index}>{product.price}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Paper>
+              );
+            })}
+          </TableBody>
+        </Table>
       </div>
     </>
   );
